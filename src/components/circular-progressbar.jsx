d8ec2173e71bd4a69322bd3c20d8db442d2a9023
PSamokat <new.samokat@gmail.com>
2023-02-08 01:58:37 +0300

    randomise values

--- src/components/circular-progressbar.jsx
+++ src/components/circular-progressbar.jsx
@@ -10,9 +10,24 @@ import  slash from '../assets/Slash.svg'
 
 const CircularProgressbar = ({props}) => {
 		
-		
+		const [predict, setPredict] = useState({current: 222555, target: 233555})
+		const [nit, setNit] = useState({current: 222555, target: 233555})
 		const changeValueHandler = () => {
-		
+				setNit({
+						current: randomInteger(200000, 230000),
+						target: randomInteger(230000, 250000),
+				}
+		);
+				setPredict(
+						{
+								current: randomInteger(200000, 230000),
+								target: randomInteger(230000, 250000),
+						}
+				);
+		}
+		function randomInteger(min, max) {
+				let rand = min + Math.random() * (max + 1 - min);
+				return Math.floor(rand);
 		}
 		function numberWithSpaces(x) {
 				return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
@@ -25,28 +40,28 @@ const CircularProgressbar = ({props}) => {
 										<div className='info__data'>
 												<div className='label'>нит</div>
 												<div className='stats'>
-														<div className='stats__current'>{numberWithSpaces(222555)}</div>
+														<div className='stats__current'>{numberWithSpaces(nit.current)}</div>
 														<div>
 																<img className='slash' src={slash}/>
 														</div>
-														<div className='stats__full'>{numberWithSpaces(233555)}</div>
+														<div className='stats__target'>{numberWithSpaces(nit.target)}</div>
 												</div>
 										</div>
 										<div className='info__data'>
 												<div className='label'>прогноз</div>
 												<div className='stats'>
-														<div className='stats__current'>{numberWithSpaces(222555)}</div>
+														<div className='stats__current'>{numberWithSpaces(predict.current)}</div>
 														<div>
 																<img className='slash' src={slash}/>
 														</div>
-														<div className='stats__full'>{numberWithSpaces(233555)}</div>
+														<div className='stats__target'>{numberWithSpaces(predict.target)}</div>
 												</div>
 										</div>
 								</div>
 								<div className='widget__chart outer'>
 										<ProgressProvider
 												valueStart={0}
-												valueEnd={96}
+												valueEnd={Math.round(nit.current / nit.target * 100)}
 										>
 												{value =>
 														<CircularProgressbarWithChildren
@@ -58,7 +73,7 @@ const CircularProgressbar = ({props}) => {
 																<div className='inner'>
 																		<ProgressProvider
 																				valueStart={0}
-																				valueEnd={95}
+																				valueEnd={Math.round(predict.current / predict.target * 100)}
 																		>
 																				{value =>
 																						<CircularProgressbarWithChildren
